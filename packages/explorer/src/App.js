5fd3c543d38024ca273fba6e2e5d076af7a19196
Allysson Santos <aallysson0@gmail.com>
2019-10-13 09:01:16 -0300

    feat(explorer): config react application with monorepo support
    re #33

---
+++ packages/explorer/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { GlobalStyle, Button, Frame, Icon } from '@react95/core';
+
+const StartupMenu = styled(Frame)`
+  position: fixed;
+  bottom: 0;
+
+  font-size: 12px;
+  font-weight: bold;
+`;
+
+const StartupButton = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2px 4px 0;
+
+  &:active {
+    padding: 3px 3px 0 5px;
+
+    box-shadow: inset 0 0 0 1px #868a8e, 0 0 0 1px #000;
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+
+      <Button
+        style={{
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+        }}
+      >
+        Ok
+      </Button>
+      <StartupMenu width="100%">
+        <StartupButton>
+          <Icon
+            name="windows95_logo"
+            style={{
+              width: 22,
+              height: 22,
+              marginRight: 4,
+            }}
+          />
+          Start
+        </StartupButton>
+      </StartupMenu>
+    </>
+  );
+}
+
+export default App;
