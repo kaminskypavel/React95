ba89c80934276a856c9aaea334403e645cb6b3ee
Allysson Santos <aallysson0@gmail.com>
2019-10-14 16:51:27 -0300

    feat(explorer): Add clock to start bar

--- packages/explorer/src/App.js
+++ packages/explorer/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
-import { GlobalStyle, Frame } from '@react95/core';
+import { GlobalStyle, Frame, Tooltip, Icon } from '@react95/core';
 
 import StartupButton from './components/StartupButton';
 import StartupList from './components/StartupList';
@@ -9,11 +9,38 @@ import ShutDown from './components/ShutDown';
 const StartupMenu = styled(Frame)`
   position: absolute;
   bottom: 0;
+  display: flex;
+  justify-content: space-between;
 `;
 
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+const getTime = date => `${date.getHours()}:${date.getMinutes()}`;
+
 function App() {
+  const [time, setTime] = useState(new Date());
+
   const [menuActive, setMenuActive] = useState(false);
   const [shutDownVisible, setShutDownVisible] = useState(false);
+
+  useInterval(() => {
+    setTime(new Date());
+  }, 1000);
 
   return (
     <>
@@ -24,6 +51,14 @@ function App() {
           onClick={() => setMenuActive(!menuActive)}
         />
         {menuActive && <StartupList setShutDownVisible={setShutDownVisible} />}
+        <Frame style={{ display: 'flex', alignItems: 'center' }} inset>
+          <Icon name="unmute" width="16" height="16" />
+          <Tooltip delay={10}>
+            <span style={{ marginLeft: 10, marginRight: 6 }}>
+              {getTime(time)}
+            </span>
+          </Tooltip>
+        </Frame>
       </StartupMenu>
       {shutDownVisible && <ShutDown setShutDownVisible={setShutDownVisible} />}
     </>
